efce42dd6c31cabc63d7a98348e2dd43624bf3e4
amg432 <168381954+amg432@users.noreply.github.com>
2024-04-30 02:22:54 +0200

    update

--- variants/NUCLEOL432KC_NOBTL/variant.h
+++ variants/NUCLEOL432KC_NOBTL/variant.h
@@ -26,8 +26,8 @@
  * WITH THE SOFTWARE.
  */
 
-#ifndef _VARIANT_ARDSTM32L432KC_
-#define _VARIANT_ARDSTM32L432KC_
+#ifndef _VARIANT_NUCLEOL432KC_NOBTL_
+#define _VARIANT_NUCLEOL432KC_NOBTL_
 
 // The definitions here needs a STM32L4 core >=1.6.6
 #define ARDUINO_STM32L4_VARIANT_COMPLIANCE 10606
@@ -39,9 +39,6 @@
 #define STM32L4_CONFIG_LSECLK             32768
 #define STM32L4_CONFIG_HSECLK             0
 #define STM32L4_CONFIG_SYSOPT             0
-#define STM32L4_CONFIG_USB_VBUS           GPIO_PIN_PA15
-
-#define USBCON
 
 /** Master clock frequency */
 #define VARIANT_MCK			  F_CPU
@@ -51,7 +48,6 @@
  *----------------------------------------------------------------------------*/
 
 #ifdef __cplusplus
-#include "USBAPI.h"
 #include "Uart.h"
 #endif // __cplusplus
 
@@ -65,15 +61,17 @@ extern "C"
  *----------------------------------------------------------------------------*/
 
 // Number of pins defined in PinDescription array
-#define PINS_COUNT           (25u)
-#define NUM_DIGITAL_PINS     (19u)
-#define NUM_TOTAL_PINS       (40u)
-#define NUM_ANALOG_INPUTS    (5u)
-#define NUM_ANALOG_OUTPUTS   (2u)
-#define analogInputToDigitalPin(p)  ((p < 6u) ? (p) + 14u : -1)
+#define PINS_COUNT           (22u)
+#define NUM_DIGITAL_PINS     (14u)
+#define NUM_TOTAL_PINS       (22u)
+#define NUM_ANALOG_INPUTS    (8u)
+#define NUM_ANALOG_OUTPUTS   (0u)
+
+#define analogInputToDigitalPin(p)  ((p < 8u) ? (p) + 14u : -1)
 
 // LEDs
 #define PIN_LED_13           (13u)
+
 #define PIN_LED              PIN_LED_13
 #define LED_BUILTIN          PIN_LED_13
 
@@ -85,51 +83,45 @@ extern "C"
 #define PIN_A2               (16u)
 #define PIN_A3               (17u)
 #define PIN_A4               (18u)
-#define PIN_DAC0             (14u)
-#define PIN_DAC1             (15u)
+#define PIN_A5               (19u)
+#define PIN_A6               (20u)
+#define PIN_A7               (21u)
+#define PIN_DAC0             (17u)
+#define PIN_DAC1             (18u)
 
-static const uint8_t A0  = PIN_A0;
-static const uint8_t A1  = PIN_A1;
-static const uint8_t A2  = PIN_A2;
-static const uint8_t A3  = PIN_A3;
-static const uint8_t A4  = PIN_A4;
+static const uint8_t A0   = PIN_A0;
+static const uint8_t A1   = PIN_A1;
+static const uint8_t A2   = PIN_A2;
+static const uint8_t A3   = PIN_A3;
+static const uint8_t A4   = PIN_A4;
+static const uint8_t A5   = PIN_A5;
+static const uint8_t A6   = PIN_A6;
+static const uint8_t A7   = PIN_A7;
 static const uint8_t DAC0 = PIN_DAC0;
 static const uint8_t DAC1 = PIN_DAC1;
+
 #define ADC_RESOLUTION		12
 #define DAC_RESOLUTION		12
-
-// Other pins
-
-#define PIN_BUTTON           (39u)
-static const uint8_t BUTTON = PIN_BUTTON;
-
-#define PIN_ATN              (38u)
-static const uint8_t ATN = PIN_ATN;
 
 /*
  * Serial interfaces
  */
 
-#define SERIAL_INTERFACES_COUNT 3
+#define SERIAL_INTERFACES_COUNT 2
 
-#define PIN_SERIAL1_RX       (0ul)
-#define PIN_SERIAL1_TX       (1ul)
+#define PIN_SERIAL_TX        (21u)
 
-#define PIN_SERIAL2_RX       (8ul)
-#define PIN_SERIAL2_TX       (9ul)
+#define PIN_SERIAL1_RX       (0u)
+#define PIN_SERIAL1_TX       (1u)
 
 /*
  * SPI Interfaces
  */
-#define SPI_INTERFACES_COUNT 2
+#define SPI_INTERFACES_COUNT 1
 
 #define PIN_SPI_MISO         (12u)
 #define PIN_SPI_MOSI         (11u)
 #define PIN_SPI_SCK          (13u)
-
-#define PIN_SPI1_MISO        (4u)
-#define PIN_SPI1_MOSI        (5u)
-#define PIN_SPI1_SCK         (3u)
 
 static const uint8_t SS	  = 10;
 static const uint8_t MOSI = PIN_SPI_MOSI;
@@ -141,33 +133,13 @@ static const uint8_t SCK  = PIN_SPI_SCK;
  */
 #define WIRE_INTERFACES_COUNT 1
 
-#define PIN_WIRE_SDA         (20u)
-#define PIN_WIRE_SCL         (21u)
-
-#define PIN_WIRE1_SDA        (4u)
-#define PIN_WIRE1_SCL        (3u)
+#define PIN_WIRE_SDA         (4u)
+#define PIN_WIRE_SCL         (5u)
 
 static const uint8_t SDA = PIN_WIRE_SDA;
 static const uint8_t SCL = PIN_WIRE_SCL;
 
-/*
- * I2S Interfaces
- */
-#define I2S_INTERFACES_COUNT 1
-
-#define PIN_I2S_SCK          (3u)
-#define PIN_I2S_FS           (PIN_A0)
-#define PIN_I2S_SD           (5u)
-#define PIN_I2S_MCK          (4u)
-
-/*
- * USB
- */
-#define PIN_USB_VBUS        (27u)
-#define PIN_USB_DM          (28u)
-#define PIN_USB_DP          (29u)
-
-#define PWM_INSTANCE_COUNT  4
+#define PWM_INSTANCE_COUNT    2
 
 #ifdef __cplusplus
 }
@@ -178,9 +150,8 @@ static const uint8_t SCL = PIN_WIRE_SCL;
  *----------------------------------------------------------------------------*/
 
 #ifdef __cplusplus
-extern CDC  Serial;
+extern Uart Serial;
 extern Uart Serial1;
-extern Uart Serial2;
 #endif
 
 // These serial port names are intended to allow libraries and architecture-neutral
@@ -198,15 +169,13 @@ extern Uart Serial2;
 //
 // SERIAL_PORT_HARDWARE_OPEN  Hardware serial ports which are open for use.  Their RX & TX
 //                            pins are NOT connected to anything by default.
-#define SERIAL_PORT_USBVIRTUAL      Serial
-#define SERIAL_PORT_MONITOR         Serial
-#define SERIAL_PORT_HARDWARE1       Serial1
-#define SERIAL_PORT_HARDWARE2       Serial2
-#define SERIAL_PORT_HARDWARE_OPEN1  Serial1
-#define SERIAL_PORT_HARDWARE_OPEN2  Serial2
+#define SERIAL_PORT_USBVIRTUAL     Serial
+#define SERIAL_PORT_MONITOR        Serial
+#define SERIAL_PORT_HARDWARE       Serial1
+#define SERIAL_PORT_HARDWARE_OPEN  Serial1
 
 // Alias SerialUSB to Serial
 #define SerialUSB SERIAL_PORT_USBVIRTUAL
 
-#endif /* _VARIANT_ARDSTM32L432KC_ */
+#endif /* _VARIANT_NUCLEOL432KC_NOBTL_ */
 
